87954975a35715f8a511b5eb3a264eaf4b95eed0
Simon Crouch <iamontheinternet@yahoo.com>
2022-01-19 15:09:48 +0000

    update github actions and add PW code

--- PlaywrightInAzureFunctionTemplate/PlaywrightDemo.cs
+++ PlaywrightInAzureFunctionTemplate/PlaywrightDemo.cs
@@ -3,6 +3,7 @@ using System.Net;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
+using Microsoft.Playwright;
 
 namespace PlaywrightInAzureFunctionTemplate
 {
@@ -16,7 +17,7 @@ namespace PlaywrightInAzureFunctionTemplate
         }
 
         [Function(nameof(PlaywrightDemo))]
-        public HttpResponseData RunPlaywrightDemo([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
+        public async Task<HttpResponseData> RunPlaywrightDemoAsync([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
 
@@ -24,6 +25,11 @@ namespace PlaywrightInAzureFunctionTemplate
             response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
 
             response.WriteString("Welcome to Azure Functions!");
+
+            using var playwright = await Playwright.CreateAsync();
+            await using var browser = await playwright.Chromium.LaunchAsync(new() { Headless = true });
+            var page = await browser.NewPageAsync();
+            await page.GotoAsync("https://playwright.dev/dotnet");
 
             return response;
         }
